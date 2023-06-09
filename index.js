1e2af0ebe174b651b104f2b49a1251fb2e119bfb
Sifatulla21 <sifat2109@gmail.com>
2023-06-09 15:14:32 +0600

    make admin api

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vmx0jtd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,7 +29,7 @@ async function run() {
     app.get('/users', async(req,res) =>{
         const result = await usersCollection.find().toArray();
         res.send(result);
-    })
+    });
 
     app.post('/users', async(req,res) =>{
         const user = req.body;
@@ -40,6 +40,17 @@ async function run() {
         }
         const result = await usersCollection.insertOne(user);
         res.send(result);
+    });
+    app.patch('users/admin/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updateDoc ={
+        $set: {
+          role: 'admin'
+        },
+      };
+      const resuly = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
     })
 
 
