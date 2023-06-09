7deeb70d049d5c9a659f8bb68c19ba70f4df5c5e
Sifatulla21 <sifat2109@gmail.com>
2023-06-09 11:18:38 +0600

    store user data using google

--- index.js
+++ index.js
@@ -28,6 +28,11 @@ async function run() {
     //users related apis
     app.post('/users', async(req,res) =>{
         const user = req.body;
+        const query = {email: user.email}
+        const existingUser = await usersCollection.findOne(query);
+        if(existingUser){
+            return res.send({message: 'User Already Exist'})
+        }
         const result = await usersCollection.insertOne(user);
         res.send(result);
     })
