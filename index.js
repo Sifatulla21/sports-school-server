9d4d38df5487e53c09c25a8ffaf30282c82fad46
Sifatulla21 <sifat2109@gmail.com>
2023-06-09 12:52:49 +0600

    make user api for get data

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ async function run() {
     const usersCollection = client.db("sportsSchool").collection("users");
 
     //users related apis
+    app.get('/users', async(req,res) =>{
+        const result = await usersCollection.find().toArray();
+        res.send(result);
+    })
+
     app.post('/users', async(req,res) =>{
         const user = req.body;
         const query = {email: user.email}
